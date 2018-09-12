a43a0eae3b81100165f39a7cae156d87f4635b0a
León Sepúlveda <NECDE@users.noreply.github.com>
2018-09-12 15:53:06 -0300

    add button - address google maps

--- basic_example.py
+++ basic_example.py
@@ -1,31 +1,39 @@
 from flask import Flask, render_template, request
-import requests,json,patentes
+import requests
+import json
+import patentes
 
 app = Flask(__name__)
 
-@app.errorhandler(403)
-def errorhandler403(e):
-	return render_template('403.html'), 403
+def errores():
+	@app.errorhandler(403)
+	def errorhandler403(e):
+		return render_template('403.html'), 403
 
-@app.errorhandler(404)
-def errorhandler404(e):
-	return render_template('404.html'), 404
+	@app.errorhandler(404)
+	def errorhandler404(e):
+		return render_template('404.html'), 404
 
-@app.errorhandler(410)
-def errorhandler410(e):
-	return render_template('410.html'), 410
+	@app.errorhandler(410)
+	def errorhandler410(e):
+		return render_template('410.html'), 410
 
-@app.errorhandler(500)
-def errorhandler500(e):
-	return render_template('500.html'), 500
+	@app.errorhandler(500)
+	def errorhandler500(e):
+		return render_template('500.html'), 500
+
+errores()
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+checkboxgoogle=0
+
 @app.route('/inforut', methods=['POST'])
 def rutpersona():
 	rut = request.form['rut_persona']
+
 	patentes.datos_persona(rut)
 
 	return render_template('inforut.html', 
@@ -39,27 +47,16 @@ def rutpersona():
 @app.route('/infopatente', methods=['POST'])
 def patenteauto():
 	patente = request.form['patente_auto']
-	# patentes.multas(patente)
-	# if patentes.multas.multass==1:
-	# 	patentes.multas.patente='Null'
-	# 	patentes.multas.rut='Null'
-	# 	patentes.multas.nombre='Null'
-	# 	patentes.multas.monto='Null'
-	# 	patentes.multas.ano='Null'
-	# 	patentes.multas.motivo='Null'
-	# 	patentes.multas.comuna='Null'
-	# else:
-	# 	pass
-	try:
-		patentes.datos_persona_auto(patente)
-	except:
-		print("404 try")
-		return render_template('404.html')
+
+	patentes.datos_persona_auto(patente)
+
+	if patentes.datos_persona_auto.rut == '0-0':
+		print("asdasdasdasdasdasd 0-0 ")
+		return render_template('errorsoap.html',error=patentes.datos_persona_auto.rut)
 
 	if patentes.datos_persona_auto.datosrutii == "{'error': 'User not found'}":
-		print("\nUser not found\n")
-		print("404 if")
-		return render_template('404.html')
+		return render_template('rutnotfoundrutify.html',error=patentes.datos_persona_auto.errorruti)
+
 	else:
 		return render_template('infopatente.html',
 							patente=patente.upper(),
@@ -75,16 +72,9 @@ def patenteauto():
 							anoauto=patentes.datos_persona_auto.anoauto,
 							nmotor=patentes.datos_persona_auto.nmotor,
 							dvpatente=patentes.datos_persona_auto.dvpatente,
-							# multa_patente=patentes.multas.patente,
-							# multa_rut=patentes.multas.rut,
-							# multa_nombre=patentes.multas.nombre,
-							# multa_monto=patentes.multas.monto,
-							# multa_ano=patentes.multas.ano,
-							# multa_motivo=patentes.multas.motivo,
-							# multa_comuna=patentes.multas.comuna,
-							primeraimagen=patentes.datos_persona_auto.primeraimagen,
-							segundaimagen=patentes.datos_persona_auto.segundaimagen,
-							terceraimagen=patentes.datos_persona_auto.terceraimagen)
+							primeraimagen=patentes.datos_persona_auto.firstimage,
+							segundaimagen=patentes.datos_persona_auto.secondimage,
+							terceraimagen=patentes.datos_persona_auto.thirdimage)
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port = 80)
+	app.run(host = '0.0.0.0', port = 32783)
--- patentes.py
+++ patentes.py
@@ -3,13 +3,14 @@ import requests
 import json
 from imagesoup import ImageSoup
 from flask import render_template
-# import webbrowser
+import webbrowser
+import basic_example
 
 def datos_persona(rut):
 
 	url = "https://api.rutify.cl/rut/%s" % rut
 	peticion = requests.get(url)
-	datosruti    = json.loads(peticion.content)
+	datosruti = json.loads(peticion.content)
 	datos_persona.nombre = datosruti["nombre"].upper()
 	datos_persona.rut = datosruti["rut"]
 	if datosruti["sexo"] == 1:
@@ -20,7 +21,79 @@ def datos_persona(rut):
 	datos_persona.domicilio = datosruti["servel"]["domicilio electoral"]
 	datos_persona.pais = datosruti["servel"]["pais"]
 
-	# webbrowser.open('https://www.google.com/maps/place/' + datos_persona.domicilio+" " +datos_persona.comuna)
+# CHECKBOX INDEX DIRECCION EN GOOGLE
+	if basic_example.request.form.get('googlemap'):
+		print("CHECKBOX - Mostrando mapa")
+
+		direccionlala = datos_persona.domicilio
+		print(direccionlala)
+		direccionlala = direccionlala.split()
+		print(direccionlala)
+
+		if len(direccionlala)>2:
+			print (" ".join(direccionlala[:3]))
+			direccionlala = (" ".join(direccionlala[:3]))
+			print("3"+direccionlala)
+			print('https://www.google.com/maps/place/' + str(direccionlala) + " " + str(datos_persona.comuna))
+
+			try:
+				webbrowser.open('https://www.google.com/maps/place/' + str(direccionlala) + " " + str(datos_persona.comuna))
+			except:
+				print("Error mostrando mapa if")
+
+		else:
+			print('https://www.google.com/maps/place/' + datos_persona.domicilio + " " + datos_persona.comuna)
+
+			try:
+				webbrowser.open('https://www.google.com/maps/place/' + datos_persona.domicilio + " " + datos_persona.comuna)
+			except:
+				print("Error mostrando mapa else")
+
+	else:
+		print("NO CHECKBOX - No mostrando mapa")
+
+
+def multas(patente):
+
+	campos = ["Patente","Rut","Nombre","Monto","Año","Motivo","Comuna",]
+	datacampos = []
+
+	url = "https://www.sem.gob.cl/pcirc/buscar_multas.php?&tipo=0&patente=%s&rut=0" % patente
+	peticion = requests.get(url)
+	peticiontxt = peticion.text
+
+	soup = BeautifulSoup(peticiontxt,'html.parser')
+	multas.multass=0
+
+	try:
+		contador=0
+		for data in soup.find_all('th'):
+			# print(data)
+			data = str(data)
+			if contador >= 7:
+				datacampos.append(data[4:-5])
+				# print(contador,data[4:-5])
+			else:
+				pass
+			contador+=1
+			# print(datacampos)
+		dictionary=dict(zip(campos,datacampos))
+		print(dictionary)
+		if len(dictionary) < 4:
+			print("No multas")
+			multas.multass=1
+		else:
+			print("OK")
+			# multas.patente=dictionary["Patente"]
+			multas.rut=dictionary["Rut"]
+			multas.nombre=dictionary["Nombre"]
+			multas.monto=dictionary["Monto"]
+			multas.ano=dictionary["Año"]
+			multas.motivo=dictionary["Motivo"]
+			multas.comuna=dictionary["Comuna"]
+
+	except:
+		print("No multas o error buscando patente")
 
 def tipoVehiculo(numero):
 
@@ -48,116 +121,109 @@ def datos_persona_auto(patente):
 	try:
 		url = "https://soap.uanbai.com/bci/soap/2018/ajax/loadPPU.jsp?PPU=%s&SES=DDB9674E703F9BB04C4F3BB2D96D8291.worker1" % patente
 		peticion = requests.get(url)
-		datos    = json.loads(peticion.content)
+		datos = json.loads(peticion.content)
 
 	except:
 		print ("\nNo se pudo obtener la información (SOAP)")
 
+	# try:
 	# Datos del dueño
+
 	datos_persona_auto.nombredueno = (datos['propietario']['nombre'] + " " + datos['propietario']['ap_paterno'] + " " + datos['propietario']['ap_materno'])
 	datos_persona_auto.rut = (datos['propietario']['rut'] +"-" + datos['propietario']['dv'])
 	
 	print(datos_persona_auto.rut)
-	if datos_persona_auto.rut == '0-0':
-		print("ERROR INFO SOAP")
-	else:
-		pass
 
 	# OBTENER DATOS DE DOMICIO ELECTORAL RUTIFY
-	url = "https://api.rutify.cl/rut/%s" % datos_persona_auto.rut
-	peticion = requests.get(url)
-	datosruti    = json.loads(peticion.content)
-	datos_persona_auto.datosrutii = str(datosruti)
-	# print(datosrutii)
 
-	datos_persona_auto.nombre = datosruti["nombre"]
-	datos_persona_auto.rut = datosruti["rut"]
-	if datosruti["sexo"] == 1:
-		datos_persona_auto.sexo = "Masculino"
-	else:
-		datos_persona_auto.sexo = "Femenino"
-	datos_persona_auto.comuna = datosruti["servel"]["comuna"]
-	datos_persona_auto.domicilio = datosruti["servel"]["domicilio electoral"]
-	datos_persona_auto.pais = datosruti["servel"]["pais"]
+	try:
+		url = "https://api.rutify.cl/rut/%s" % datos_persona_auto.rut
+		peticion = requests.get(url)
+		datosruti = json.loads(peticion.content)
+		datos_persona_auto.datosrutii = str(datosruti)
+
+		
+
+		# print(datos_persona_auto.datosrutii)
+
+		datos_persona_auto.nombre = datosruti["nombre"]
+		datos_persona_auto.rut = datosruti["rut"]
+		if datosruti["sexo"] == 1:
+			datos_persona_auto.sexo = "Masculino"
+		else:
+			datos_persona_auto.sexo = "Femenino"
+		datos_persona_auto.comuna = datosruti["servel"]["comuna"]
+		datos_persona_auto.domicilio = datosruti["servel"]["domicilio electoral"]
+		datos_persona_auto.pais = datosruti["servel"]["pais"]
+
+
+# CHECKBOX INDEX DIRECCION EN GOOGLE
+
+		if basic_example.request.form.get('googlemap'):
+			print("CHECKBOX - Mostrando mapa")
+
+			direccionlala = datos_persona_auto.domicilio
+			print(direccionlala)
+			direccionlala = direccionlala.split()
+			print(direccionlala)
+
+			if len(direccionlala)>2:
+				print (" ".join(direccionlala[:3]))
+				direccionlala = (" ".join(direccionlala[:3]))
+				print("3"+direccionlala)
+				print('https://www.google.com/maps/place/' + str(direccionlala) + " " + str(datos_persona_auto.comuna))
+
+				try:
+					webbrowser.open('https://www.google.com/maps/place/' + str(direccionlala) + " " + str(datos_persona_auto.comuna))
+				except:
+					print("Error mostrando mapa if")
+
+			else:
+				print('https://www.google.com/maps/place/' + datos_persona_auto.domicilio + " " + datos_persona_auto.comuna)
+
+				try:
+					webbrowser.open('https://www.google.com/maps/place/' + datos_persona_auto.domicilio + " " + datos_persona_auto.comuna)
+				except:
+					print("Error mostrando mapa else")
+
+		else:
+			print("NO CHECKBOX - No mostrando mapa")
+
+
+	except:
+		datos_persona_auto.errorruti=datosruti["error"]
+		print("Error sacando info de rutify")
 
 
 	# Datos del vehiculo
-	
-	datos_persona_auto.tipovehi = (tipoVehiculo(datos['id_tipo']))
-	datos_persona_auto.marcavehi = (datos['marca'])
-	datos_persona_auto.modelovehi = (datos['modelo'])
-	datos_persona_auto.anoauto = (str(datos['ano']))
-	datos_persona_auto.nmotor = (str(datos['vin']))
-	datos_persona_auto.dvpatente = (str(datos['dvpatente']))
-
-	marca = datos_persona_auto.marcavehi
-	modelo = datos_persona_auto.modelovehi
-	ano = datos_persona_auto.anoauto
-
-	busqueda = (marca,modelo,ano)
-
-	auto = ImageSoup()
-	images = auto.search(busqueda, n_images=5)
-
-	firstimage = images[0]
-	firstimage = str(firstimage)
-
-	secondimage = images[1]
-	secondimage = str(secondimage)
-
-	thirdimage = images[2]
-	thirdimage = str(thirdimage)
-
-	datos_persona_auto.primeraimagen = firstimage[13:-2]
-	datos_persona_auto.segundaimagen = secondimage[13:-2]
-	datos_persona_auto.terceraimagen = thirdimage[13:-2]
-
-	# print(datos_persona_auto.primeraimagen)
-
-	# print(datos_persona_auto.primeraimagen,datos_persona_auto.segundaimagen,datos_persona_auto.terceraimagen)
-
-def multas(patente):
-
-	campos = ["Patente","Rut","Nombre","Monto","Año","Motivo","Comuna",]
-	datacampos = []
-
-	url = "https://www.sem.gob.cl/pcirc/buscar_multas.php?&tipo=0&patente=%s&rut=0" % patente
-	peticion = requests.get(url)
-	peticiontxt    = peticion.text
-
-	soup = BeautifulSoup(peticiontxt,'html.parser')
-	multas.multass=0
-
 	try:
-		contador=0
-		for data in soup.find_all('th'):
-			# print(data)
-			data = str(data)
-			if contador >= 7:
-				datacampos.append(data[4:-5])
-				# print(contador,data[4:-5])
-			else:
-				pass
-			contador+=1
-			# print(datacampos)
-		dictionary=dict(zip(campos,datacampos))
-		print(dictionary)
-		if len(dictionary) < 4:
-			print("No multas")
-			multas.multass=1
-		else:
-			print("OK")
-			multas.patente=dictionary["Patente"]
-			multas.rut=dictionary["Rut"]
-			multas.nombre=dictionary["Nombre"]
-			multas.monto=dictionary["Monto"]
-			multas.ano=dictionary["Año"]
-			multas.motivo=dictionary["Motivo"]
-			multas.comuna=dictionary["Comuna"]
+
+		datos_persona_auto.tipovehi = (tipoVehiculo(datos['id_tipo']))
+		
+		print(datos_persona_auto.tipovehi)
+
+		datos_persona_auto.marcavehi = (datos['marca'])
+		datos_persona_auto.modelovehi = (datos['modelo'])
+		datos_persona_auto.anoauto = (str(datos['ano']))
+		datos_persona_auto.nmotor = (str(datos['vin']))
+		datos_persona_auto.dvpatente = (str(datos['dvpatente']))
+
+
+		# Busqueda de imagenes en google del modelo del auto
+		# Problema con mostrar imagenes de webs sin https
+
+		busqueda = (datos_persona_auto.marcavehi,datos_persona_auto.modelovehi,datos_persona_auto.anoauto)
+
+		auto = ImageSoup()
+
+		images = auto.search(busqueda, n_images=4)
+
+		datos_persona_auto.firstimage = images[0].URL
+		datos_persona_auto.secondimage = images[1].URL
+		datos_persona_auto.thirdimage = images[2].URL
 
 	except:
-		print("No multas o error buscando patente")
+		print("datos auto error")
 
-
-
-# multas("DSFS12")
+	# except:
+	# 	print("Error dato info de Persona")
